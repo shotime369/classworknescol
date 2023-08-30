79c698898830cb0cca63508cd15c2db3924c015b
shotime369 <27260255@nescol.ac.uk>
2023-08-30 11:08:55 +0100

    Add Hello World

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * My First Program
+ * @author shona penny
+ * @version 1
+ */
+
+public class HelloWorld {
+
+        public static void main (String[] args){
+            //write out Hello World to the console
+            System.out.println("Hello World");
+        }
+}
