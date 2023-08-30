aa3d6063796e131232dbd492acb91dde2c967605
shotime369 <27260255@nescol.ac.uk>
2023-08-30 11:34:39 +0100

    Add Hello World

---
+++ src/L1E1haiku.java
@@ -0,0 +1,8 @@
+public class L1E1haiku {
+
+    public static void main(String[] args) {
+        System.out.println("Flame-grilled whispers,");
+        System.out.println("Burger King's latest tale told,");
+        System.out.println("Savory headlines.");
+    }
+}
