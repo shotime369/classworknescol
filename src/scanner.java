845f1677717b266b7cd2999dfd3baae55b741bcd
shotime369 <27260255@nescol.ac.uk>
2023-09-06 09:53:39 +0100

    ANAGRAM FUN

---
+++ src/scanner.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class scanner {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("what is your name? ");
+        String name = input.nextLine();
+
+    }
+}
