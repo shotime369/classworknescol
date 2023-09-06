5f1ba9dc4239478ff0b1efc017168f7e6ca46d94
shotime369 <27260255@nescol.ac.uk>
2023-09-06 09:23:15 +0100

    ANAGRAM FUN

---
+++ src/lesson2.java
@@ -0,0 +1,8 @@
+public class multiple lines
+{
+
+        public static void main (String[] args){
+            //create multiple lines with \n
+            System.out.println("Hickory dickory dock!\nthe mouse ran up the clock");
+        }
+    }
