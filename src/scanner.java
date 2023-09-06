fcd8afa89bfcb0f9dd3d925525b6205075f0c88d
shotime369 <27260255@nescol.ac.uk>
2023-09-06 10:09:56 +0100

    ANAGRAM FUN

--- src/scanner.java
+++ src/scanner.java
@@ -5,6 +5,10 @@ public class scanner {
         Scanner input = new Scanner(System.in);
         System.out.print("what is your name? ");
         String name = input.nextLine();
+        System.out.print("what is your quest? ");
+        String quest = input.nextLine();
+
+        System.out.println("good luck " + name + " with your " + quest);
 
     }
 }
