3c07fbc3b5d7d877209697849d9b8033abd5541e
Shotime369 <27260255@nescol.ac.uk>
2023-09-07 10:26:12 +0100

    homework

--- src/numbers.java
+++ src/numbers.java
@@ -4,25 +4,21 @@ public class numbers {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+        //ask for integer called num
         System.out.print("first number ");
         int num = input.nextInt();
-
+        //ask for input integer called number
         System.out.print("second number ");
         int number = input.nextInt();
-
-        System.out.print ("Sum = ");
-        System.out.println (num + number);
-        System.out.print ("Subtract = ");
-        System.out.println (num - number);
-        System.out.print ("Multipy= ");
-        System.out.println (num * number);
-        System.out.print ("Divide= ");
-float Divresult = (float) num / (float) number;
-        System.out.print(Divresult);
-        System.out.print ("Modulo= ");
-        System.out.println (num % number);
-       // System.out.print ("Exponent= ");
-      //  System.out.println (num % number);
+//print out sums
+        System.out.println ("Sum = " + (num + number));
+        System.out.println ("Subtract = "+ (num - number));
+        System.out.println ("Multipy= " + (num * number));
+        //float the decimal result from division
+        float Divresult = (float) num / (float) number;
+        System.out.println ("Divide= "+ (Divresult));
+        System.out.println ("Modulo= "+ (num % number));
+        System.out.println ("Exponent= " + (Math.pow(num,number)));
 
 
     }
