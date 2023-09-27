44a9c4ee65dd7b26a0bc30e65b77b19bb8cb6ebd
Shotime369 <27260255@nescol.ac.uk>
2023-09-27 16:11:10 +0100

    lesson 5e3 added choice

--- src/L5E3.java
+++ src/L5E3.java
@@ -6,42 +6,77 @@ public class L5E3 {
 
     public static void main(String[] args) {
         System.out.println("Maths Game!");
-
-        //start score
-        int score = 0;
-
-                //define number of loops for questions
-        for (int q = 1 ; q <= 10; q++) {
-            //add new random class
-            Random rand = new Random();
-            //generate 2 random numbers
-            int r1 = rand.nextInt(20);
-            int r2 = rand.nextInt(20);
-            //create correct answer
-            int correct = (r1 + r2);
-            //display question
-            System.out.print("Question " + q + ": ");
-            System.out.print("What is "+ r1+ " + "+ r2+ "? = ");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Choose Addition (1) or Subtraction (2) = ");
+        int menu = input.nextInt();
+        if (menu == 1){
+            //start score
+            int score = 0;
+            //define number of loops for questions
+            for (int q = 1; q <= 10; q++) {
+                //add new random class
+                Random rand = new Random();
+                //generate 2 random numbers
+                int r1 = rand.nextInt(20);
+                int r2 = rand.nextInt(20);
+                //create correct answer
+                int correct = (r1 + r2);
+                //display question
+                System.out.print("Question " + q + ": ");
+                System.out.print("What is " + r1 + " + " + r2 + "? = ");
                 int ans1 = sc.nextInt();
                 //if you are correct
-               if (ans1 == correct)
-               {
-              System.out.println("Correct!");
-                   score++;
-                   System.out.println("Your score is " + score + "\n");
+                if (ans1 == correct) {
+                    System.out.println("Correct!");
+                    score++;
+                    System.out.println("Your score is " + score + "\n");
                 }
-                    // else you get it wrong
-            else System.out.println("False \n");
-               }
-               //print final score
-        System.out.println("Your score is = " + score);
-        if (score >= 7) {
-            System.out.println("Great job!");
-        } else {
-            System.out.println("Keep practicing!");
+                // else you get it wrong
+                else System.out.println("False \n");
+            }
+            //print final score
+            System.out.println("Your score is = " + score);
+            if (score >= 7) {
+                System.out.println("Great job!");
+            } else {
+                System.out.println("Keep practicing!");
+            }
+        } else if (menu == 2) {
+            //start score
+            int score = 0;
+            //define number of loops for questions
+            for (int q = 1; q <= 10; q++) {
+                //add new random class
+                Random rand = new Random();
+                //generate 2 random numbers
+                int r1 = rand.nextInt(20);
+                int r2 = rand.nextInt(20);
+                //create correct answer
+                int correct = (r1 - r2);
+                //display question
+                System.out.print("Question " + q + ": ");
+                System.out.print("What is " + r1 + " - " + r2 + "? = ");
+                int ans1 = sc.nextInt();
+                //if you are correct
+                if (ans1 == correct) {
+                    System.out.println("Correct!");
+                    score++;
+                    System.out.println("Your score is " + score + "\n");
+                }
+                // else you get it wrong
+                else System.out.println("False \n");
+            }
+            //print final score
+            System.out.println("Your score is = " + score);
+            if (score >= 7) {
+                System.out.println("Great job!");
+            } else {
+                System.out.println("Keep practicing!");
+            }
+
         }
-          }
-      }
+    }
+}
 
 
 
