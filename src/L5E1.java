89dc8d61328e0172e7ef9add11153230dbb0fedd
Shotime369 <27260255@nescol.ac.uk>
2023-09-28 15:03:27 +0100

    lesson 5e3 added choice

--- src/L5E1.java
+++ src/L5E1.java
@@ -6,8 +6,8 @@ public class L5E1 {
     public static void main(String[] args) {
         System.out.print("Which number to begin: ");
         int number = sc.nextInt();
-        for (int times = number; times <= (number * 12); times += number) {
-            System.out.println(" " + times);
+        for (int times = 1; times <= 12; times ++) {
+
             System.out.format("%d x %d = %d %n", number, times, (number * times) );
         }
 
