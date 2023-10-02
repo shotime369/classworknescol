f5f3783b8c49514a2031e78f86f1e481a8994ab0
Shotime369 <27260255@nescol.ac.uk>
2023-10-02 21:56:36 +0100

    guess game

---
+++ src/extra5b.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class extra5b {
+
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        int r1 = rand.nextInt(10);
+        Scanner input = new Scanner(System.in);
+        System.out.print("Guess a number = ");
+
+        for (int q = 1; q <= 5; q++) {
+
+        int num1 = input.nextInt();
+        if (num1 == r1) {
+            System.out.println("Correct!");
+        } else {
+            System.out.println("try again");
+        }
+        }
+        System.out.println("The correct answer was " + r1);
+    }
+}
