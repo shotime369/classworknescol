64d50e260cc7398dffded871ebf41d5499d957ec
Shotime369 <27260255@nescol.ac.uk>
2023-10-02 22:15:55 +0100

    I feel like this is where I need to learn about arrays...

---
+++ src/extra5c.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class extra5c {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number (1-5) = ");
+        int num1 = input.nextInt();
+        System.out.print("Enter a number (1-5) = ");
+        int num2 = input.nextInt();
+        System.out.print("Enter a number (1-5) = ");
+        int num3 = input.nextInt();
+
+        for (int q = 1; q <= 20; q++) {
+        if (num1 == 1) {
+            System.out.print("*");
+        } else if (num1 == 2) {
+            System.out.print("_");
+        } else if (num1 == 3) {
+            System.out.print("=");
+        } else if (num1 == 4) {
+            System.out.print("/");
+        } else if (num1 == 5) {
+            System.out.print(".");
+        } else {
+            System.out.println(" ");
+        }
+
+        if (num2 == 1) {
+                System.out.print("*");
+            } else if (num2 == 2) {
+                System.out.print("_");
+            } else if (num2 == 3) {
+                System.out.print("=");
+            } else if (num2 == 4) {
+                System.out.print("/");
+            } else if (num2 == 5) {
+                System.out.print(".");
+        } else {
+            System.out.println(" ");
+        }
+
+
+       if (num3 == 1) {
+            System.out.print("*");
+        } else if (num3 == 2) {
+            System.out.print("_");
+        } else if (num3 == 3) {
+            System.out.print("=");
+        } else if (num3 == 4) {
+            System.out.print("/");
+        } else if (num3 == 5) {
+            System.out.print(".");
+        } else {
+            System.out.println(" ");
+        }
+        }
+    }
+}
