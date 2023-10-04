b80f8b3e694096f20c1e1bce28178c7ab2d3b162
shotime369 <27260255@nescol.ac.uk>
2023-10-04 10:04:17 +0100

    Lesson 5

---
+++ src/L6E1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class L6E1 {
+    public static void main(String[] args) {
+
+        int count = 10;
+        while (count <= 24) {
+            System.out.println(count);
+            count += 2;
+
+            //num % 2 != 0
+        }
+    }
+}
