efe575c711c447d4afa0111463da80857110dc9a
shotime369 <27260255@nescol.ac.uk>
2023-11-01 11:40:57 +0000

    lesson 7

---
+++ src/L8E1_array.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class L8E1_array {
+    public static void main(String[] args) {
+        String[] classlist = new String[5];
+        classlist[0] = "Steve";
+        classlist[1] = "Jane";
+        classlist[2] = "Bob";
+        classlist[3] = "Laura";
+        classlist[4] = "William";
+
+        System.out.println(classlist[0]);
+        System.out.println(classlist[4]);
+        System.out.println( classlist.length );
+    }
+}
