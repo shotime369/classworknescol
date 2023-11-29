f17aa693e8e8498eb145664022b4926f2dcdf46d
shotime369 <27260255@nescol.ac.uk>
2023-11-29 09:44:37 +0000

    l12

---
+++ src/L12E1.java
@@ -0,0 +1,16 @@
+
+
+public class L12E1 {
+    public static void displayStudent(Student thisStudent){
+        System.out.format("(%d) %s %s %n",thisStudent.id, thisStudent.firstName, thisStudent.surname);
+    }
+    public static void main(String[] args) {
+        // create student object and display
+        Student student1 = new Student();
+        displayStudent(student1);
+        // update values of student object and display
+
+        Student A = new Student();
+
+    }
+}
